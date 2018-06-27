a585b7da2fa378eb3872312c9f02e6e73e25c24d
waqqas <waqqas.jabbar@gmail.com>
2018-06-27 13:58:23 +0500

    Added Task Redux

--- App/Redux/TasksRedux.js
+++ App/Redux/TasksRedux.js
@@ -1,10 +1,11 @@
 import {createActions, createReducer} from "reduxsauce";
 import Immutable from "seamless-immutable";
+import update from 'immutability-helper';
 
 /* ------------- Types and Action Creators ------------- */
 
-const { Types, Creators } = createActions({
-    addTask: ['task'],
+const {Types, Creators} = createActions({
+  addTask: ['task'],
 })
 
 export const TasksTypes = Types
@@ -13,17 +14,17 @@ export default Creators
 /* ------------- Initial State ------------- */
 
 export const INITIAL_STATE = Immutable({
-    list: [],
+  list: [],
 })
 
 /* ------------- Reducers ------------- */
 
-export const addTask = (state) => state.merge({success: true})
+export const addTask = (state, {task}) => state.merge({list: update(state.list, {$push: task})})
 
 /* ------------- Hookup Reducers To Types ------------- */
 
 export const reducer = createReducer(INITIAL_STATE, {
-    [Types.ADD_TASK]: addTask,
+  [Types.ADD_TASK]: addTask,
 })
 
 /* ------------- Selector ------------- */
