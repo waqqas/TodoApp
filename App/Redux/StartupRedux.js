c96d3db6109927744f71685321339089cd9da3af
waqqas <waqqas.jabbar@gmail.com>
2018-06-27 12:28:35 +0500

    Removed unused code

--- App/Redux/StartupRedux.js
+++ App/Redux/StartupRedux.js
@@ -1,11 +1,9 @@
 import {createActions, createReducer} from "reduxsauce";
 import Immutable from "seamless-immutable";
-import _ from 'lodash'
 
 /* ------------- Types and Action Creators ------------- */
 
 const { Types, Creators } = createActions({
-    logoutUserSuccess: ['response'],
     startup: null,
     startupSuccess: null,
     startupFailure: null,
@@ -22,8 +20,6 @@ export const INITIAL_STATE = Immutable({
 
 /* ------------- Reducers ------------- */
 
-export const logoutUserSuccess = (state) => state.merge(_.omit(INITIAL_STATE, ['success']))
-
 export const startupSuccess = (state) => state.merge({success: true})
 
 export const startupFailure = (state) => state.merge({success: false})
@@ -31,7 +27,6 @@ export const startupFailure = (state) => state.merge({success: false})
 /* ------------- Hookup Reducers To Types ------------- */
 
 export const reducer = createReducer(INITIAL_STATE, {
-    [Types.LOGOUT_USER_SUCCESS]: logoutUserSuccess,
     [Types.STARTUP_SUCCESS]: startupSuccess,
     [Types.STARTUP_FAILURE]: startupFailure,
 })
