c566ecc2caa709ffb3ea9491df5e1d73fa08d485
waqqas <waqqas.jabbar@gmail.com>
2018-06-29 16:45:00 +0500

    Testing pre-commit hook

--- App/Models/index.js
+++ App/Models/index.js
@@ -5,7 +5,6 @@ export type Navigator = {
   navigate: (routeName: string, params?: any, action?: any, key?: string) => void,
   pop: (key?: string) => void,
   setParams: (params: any, key?: string) => void,
-  getParam: (string, any) => void,
   replace: (key: string, newKey?: string, routeName?: string, params?: any) => void
 }
 
