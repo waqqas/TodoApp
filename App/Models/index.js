ea1b16884b0a6591b9854c8f4051ec74bf9d9b2a
waqqas <waqqas.jabbar@gmail.com>
2018-06-29 16:39:03 +0500

    Added getParam function in Navigator description

--- App/Models/index.js
+++ App/Models/index.js
@@ -5,6 +5,7 @@ export type Navigator = {
   navigate: (routeName: string, params?: any, action?: any, key?: string) => void,
   pop: (key?: string) => void,
   setParams: (params: any, key?: string) => void,
+  getParam: (string, any) => void,
   replace: (key: string, newKey?: string, routeName?: string, params?: any) => void
 }
 
